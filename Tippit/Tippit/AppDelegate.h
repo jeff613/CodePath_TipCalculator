4d0f785ce084ea7281683b68d658d44b0c3188ef
Jianfeng Ye <jye@zynga.com>
2015-01-14 18:13:16 -0800

    Create empty project

---
+++ Tippit/Tippit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tippit
+//
+//  Created by Jianfeng Ye on 1/14/15.
+//  Copyright (c) 2015 Jianfeng Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tippit/Tippit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tippit
+//
+//  Created by Jianfeng Ye on 1/14/15.
+//  Copyright (c) 2015 Jianfeng Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
